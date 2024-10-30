b1417f171bb7719a876a817918d638c036de2423
Vincent Mahnke <vincent@mahn.ke>
2024-10-30 16:09:49 +0100

    feat(Actions): Adds `&LOCK_MOVE_CHOICE` and `&LOCK_TALK_CHOICE`

--- unity-ggjj/Assets/Scripts/TextDecoder/Parser/ChoiceTypeParser.cs
+++ unity-ggjj/Assets/Scripts/TextDecoder/Parser/ChoiceTypeParser.cs
@@ -2,13 +2,13 @@ using System;
 
 namespace TextDecoder.Parser
 {
-    public class ChoiceTypeParser : Parser<InvestigationState.ChoiceType>
+    public class ChoiceTypeParser : Parser<IInvestigationState.ChoiceType>
     {
-        public override string Parse(string input, out InvestigationState.ChoiceType output)
+        public override string Parse(string input, out IInvestigationState.ChoiceType output)
         {
             if (!Enum.TryParse(input, out output))
             {
-                return $"Cannot convert '{input}' into an {typeof(InvestigationState.ChoiceType)} (valid values include: '{string.Join(", ", Enum.GetValues(typeof(InvestigationState.ChoiceType)))}')";
+                return $"Cannot convert '{input}' into an {typeof(IInvestigationState.ChoiceType)} (valid values include: '{string.Join(", ", Enum.GetValues(typeof(IInvestigationState.ChoiceType)))}')";
             }
             return null;
         }
