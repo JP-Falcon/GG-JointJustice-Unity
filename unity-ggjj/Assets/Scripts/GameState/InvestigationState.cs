b7d0a7785bc15e3fea63125b068e62667683f2d9
Vincent Mahnke <vincent@mahn.ke>
2024-10-31 13:19:37 +0100

    refactor: Replaced multiple choice state containers with one

--- unity-ggjj/Assets/Scripts/GameState/InvestigationState.cs
+++ unity-ggjj/Assets/Scripts/GameState/InvestigationState.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +9,6 @@ using UnityEngine.UI;
 
 public class InvestigationState : MonoBehaviour, IInvestigationState
 {
-
     [SerializeField] private MenuOpener _investigationMainMenuOpener;
     [SerializeField] private ChoiceMenu _investigationTalkMenu;
     [SerializeField] private InvestigationChoiceMenu _investigationMoveMenu;
@@ -18,69 +16,62 @@ public class InvestigationState : MonoBehaviour, IInvestigationState
     [SerializeField] private InputManager _inputManager;
     [SerializeField] private InputModule _gameInputModule;
     [SerializeField] private InputModule _investigationInputModule;
-    
+
     #region Talk + Move
     [Header("Move")]
     [SerializeField] private Texture2D _unestablishedSceneBackground;
 
     private List<Choice> _talkOptions;
     private List<Choice> _moveOptions;
-    
-    private readonly List<string> _explicitlyLockedTalkChoices = new();
-    private readonly List<string> _explicitlyLockedMoveChoices = new();
-    private readonly List<string> _unlockedTalkChoices = new();
-    private readonly List<string> _unlockedMoveChoices = new();
-    private readonly List<string> _examinedTalkChoices = new();
-    private readonly List<string> _examinedMoveChoices = new();
-    private readonly List<string> _examinedDetails = new();
+
+    private readonly List<ChoiceState> _choiceStates = new();
+
+    private ChoiceState GetOrCreateChoiceState(string choice, IInvestigationState.ChoiceType type)
+    {
+        var choiceState = _choiceStates.FirstOrDefault(cs => cs.Text == choice && cs.Type == type);
+        if (choiceState != null)
+        {
+            return choiceState;
+        }
+        choiceState = new ChoiceState(choice, type);
+        _choiceStates.Add(choiceState);
+        return choiceState;
+    }
+
+    private record ChoiceState(string Text, IInvestigationState.ChoiceType Type)
+    {
+        public string Text { get; } = Text;
+        public IInvestigationState.ChoiceType Type { get; } = Type;
+        public bool Examined { get; set; }
+        public bool Unlocked { get; set; }
+        public bool ExplicitlyLocked { get; set; }
+    }
 
     public void UnlockChoice(string choice, IInvestigationState.ChoiceType type)
     {
-        switch (type)
-        {
-            case IInvestigationState.ChoiceType.Talk:
-                _unlockedTalkChoices.Add(choice);
-                _explicitlyLockedTalkChoices.Remove(choice);
-                break;
-            case IInvestigationState.ChoiceType.Move:
-                _unlockedMoveChoices.Add(choice);
-                _explicitlyLockedMoveChoices.Remove(choice);
-                break;
-            default:
-                throw new ArgumentOutOfRangeException(nameof(type), type, null);
-        }
+        var choiceState = GetOrCreateChoiceState(choice, type);
+        choiceState.Unlocked = true;
+        choiceState.ExplicitlyLocked = false;
     }
-    
+
     public void LockChoice(string choice, IInvestigationState.ChoiceType type)
     {
-        switch (type)
-        {
-            case IInvestigationState.ChoiceType.Talk:
-                _unlockedTalkChoices.Remove(choice);
-                _explicitlyLockedTalkChoices.Add(choice);
-                break;
-            case IInvestigationState.ChoiceType.Move:
-                _unlockedMoveChoices.Remove(choice);
-                _explicitlyLockedMoveChoices.Add(choice);
-                break;
-            default:
-                throw new ArgumentOutOfRangeException(nameof(type), type, null);
-        }
+        var choiceState = GetOrCreateChoiceState(choice, type);
+        choiceState.Unlocked = false;
+        choiceState.ExplicitlyLocked = true;
     }
 
     public bool IsChoiceUnlocked(string choice, IInvestigationState.ChoiceTag choiceTag, IInvestigationState.ChoiceType type)
     {
-        return type switch
+        var choiceState = _choiceStates.FirstOrDefault(cs => cs.Text == choice && cs.Type == type);
+        if (choiceState == null)
         {
-            // return true if: choicetag locked and inside unlocked choices or choicetag none and not explicitly locked
-            IInvestigationState.ChoiceType.Talk => choiceTag == IInvestigationState.ChoiceTag.Locked
-                ? _unlockedTalkChoices.Contains(choice)
-                : !_explicitlyLockedTalkChoices.Contains(choice),
-            IInvestigationState.ChoiceType.Move => choiceTag == IInvestigationState.ChoiceTag.Locked
-                ? _unlockedMoveChoices.Contains(choice)
-                : !_explicitlyLockedMoveChoices.Contains(choice),
-            _ => false
-        };
+            return choiceTag != IInvestigationState.ChoiceTag.Locked;
+        }
+
+        return choiceTag == IInvestigationState.ChoiceTag.Locked
+            ? choiceState.Unlocked
+            : !choiceState.ExplicitlyLocked;
     }
 
     public void OpenWithChoices(List<Choice> talkOptions, List<Choice> moveOptions)
@@ -89,30 +80,30 @@ public class InvestigationState : MonoBehaviour, IInvestigationState
         _moveOptions = moveOptions;
         StartCoroutine(OpenMenuDelayed());
     }
-    
+
     private IEnumerator OpenMenuDelayed()
     {
         yield return null;
         _investigationMainMenuOpener.OpenMenu();
     }
-    
+
     public void OpenTalkMenu()
     {
-        // Immediately play initial dialogue, if available and not yet examined
         var choiceTaggedInitial = _talkOptions.FirstOrDefault(choice => choice.tags != null && choice.tags.Contains("Initial"));
         if (choiceTaggedInitial != null)
         {
-            if (!_examinedTalkChoices.Contains(_narrativeGameState.SceneController.ActiveSceneName + "_" + choiceTaggedInitial.text))
+            var choiceState = _choiceStates.FirstOrDefault(cs => cs.Text == choiceTaggedInitial.text && cs.Type == IInvestigationState.ChoiceType.Talk);
+            if (choiceState == null || !choiceState.Examined)
             {
                 _inputManager.SetInput(_gameInputModule);
                 _investigationMainMenuOpener.CloseMenu();
                 _narrativeGameState.NarrativeScriptPlayerComponent.NarrativeScriptPlayer.HandleChoice(0);
-                _examinedTalkChoices.Add(_narrativeGameState.SceneController.ActiveSceneName + "_" + choiceTaggedInitial.text);
+                choiceState = GetOrCreateChoiceState(choiceTaggedInitial.text, IInvestigationState.ChoiceType.Talk);
+                choiceState.Examined = true;
                 return;
             }
         }
-        
-        // Open choice menu
+
         _investigationMainMenuOpener.CloseMenu();
         _investigationTalkMenu.Initialise(_talkOptions, () =>
         {
@@ -120,15 +111,16 @@ public class InvestigationState : MonoBehaviour, IInvestigationState
             OpenWithChoices(_talkOptions, _moveOptions);
         }, (menuItem) =>
         {
-            menuItem.transform.Find("AlreadyExamined").gameObject.SetActive(_examinedTalkChoices.Contains(_narrativeGameState.SceneController.ActiveSceneName + "_" + menuItem.Text));
+            var choiceState = GetOrCreateChoiceState(menuItem.Text, IInvestigationState.ChoiceType.Talk);
+            menuItem.transform.Find("AlreadyExamined").gameObject.SetActive(choiceState.Examined);
             menuItem.GetComponent<Button>().onClick.AddListener(() =>
             {
                 if (menuItem.Text == ChoiceMenu.BACK_BUTTON_LABEL)
                 {
                     return;
                 }
-                
-                _examinedTalkChoices.Add(_narrativeGameState.SceneController.ActiveSceneName + "_" + menuItem.Text);
+
+                choiceState.Examined = true;
             });
         });
     }
@@ -144,6 +136,7 @@ public class InvestigationState : MonoBehaviour, IInvestigationState
             OpenWithChoices(_talkOptions, _moveOptions);
         }, menuItem =>
         {
+            var choiceState = GetOrCreateChoiceState(menuItem.Text, IInvestigationState.ChoiceType.Move);
             menuItem.ShouldIgnoreNextSelectEvent = false;
             menuItem.GetComponent<Button>().onClick.AddListener(() =>
             {
@@ -152,7 +145,7 @@ public class InvestigationState : MonoBehaviour, IInvestigationState
                     return;
                 }
 
-                _examinedMoveChoices.Add(_narrativeGameState.SceneController.ActiveSceneName + "_" + menuItem.Text);
+                choiceState.Examined = true;
                 _investigationMoveMenu.transform.parent.gameObject.SetActive(false);
             });
             menuItem.OnItemSelect.AddListener(() =>
@@ -164,8 +157,7 @@ public class InvestigationState : MonoBehaviour, IInvestigationState
 
                 _investigationMoveMenu.SceneImage.color = Color.white;
 
-                // if unexamined, show _unestablishedSceneBackground
-                if (!_examinedMoveChoices.Contains(_narrativeGameState.SceneController.ActiveSceneName + "_" + menuItem.Text))
+                if (!choiceState.Examined)
                 {
                     _investigationMoveMenu.SceneImage.sprite = Sprite.Create(_unestablishedSceneBackground, new Rect(0, 0, _unestablishedSceneBackground.width, _unestablishedSceneBackground.height), new Vector2(0.5f, 0.5f));
                     return;
@@ -182,8 +174,8 @@ public class InvestigationState : MonoBehaviour, IInvestigationState
                 var sprite = rootPrefab.transform.Find("Background").GetComponent<SpriteRenderer>().sprite;
                 _investigationMoveMenu.SceneImage.sprite = sprite;
             });
-                
-            if(menuItem.transform.GetSiblingIndex() == 0)
+
+            if (menuItem.transform.GetSiblingIndex() == 0)
             {
                 menuItem.Selectable.Select();
             }
@@ -205,9 +197,9 @@ public class InvestigationState : MonoBehaviour, IInvestigationState
         {
             return;
         }
-        
+
         _inputManager.SetInput(_gameInputModule);
-        
+
         Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
 
         _narrativeGameState.ActorController.SetVisibility(true, null);
@@ -215,10 +207,12 @@ public class InvestigationState : MonoBehaviour, IInvestigationState
         _narrativeGameState.NarrativeScriptPlayerComponent.NarrativeScriptPlayer.ActiveNarrativeScriptPlayer.OnNarrativeScriptComplete += () =>
         {
             _narrativeGameState.AppearingDialogueController.TextBoxHidden = true;
-            _examinedDetails.Add($"{_narrativeGameState.SceneController.ActiveSceneName}_{_hoveredDetail.NarrativeScriptToPlay.name}");
+            var detailKey = $"{_narrativeGameState.SceneController.ActiveSceneName}_{_hoveredDetail.NarrativeScriptToPlay.name}";
+            var choiceState = GetOrCreateChoiceState(detailKey, IInvestigationState.ChoiceType.Examine);
+            choiceState.Examined = true;
             _hoveredDetail.AttemptPickUp();
             _narrativeGameState.ActorController.SetVisibility(true, null);
-        }; 
+        };
     }
 
     public void OnCursorMove(InputAction.CallbackContext context)
@@ -232,17 +226,20 @@ public class InvestigationState : MonoBehaviour, IInvestigationState
         {
             return;
         }
-        
+
         _hoveredDetail = hit.transform.GetComponent<Detail>();
-        
+
+        var detailKey = $"{_narrativeGameState.SceneController.ActiveSceneName}_{_hoveredDetail.NarrativeScriptToPlay.name}";
+        var choiceState = GetOrCreateChoiceState(detailKey, IInvestigationState.ChoiceType.Examine);
+
         Cursor.SetCursor(_examinationNewEvidence, Vector2.zero, CursorMode.Auto);
-        
-        if (_examinedDetails.Contains($"{_narrativeGameState.SceneController.ActiveSceneName}_{_hoveredDetail.NarrativeScriptToPlay.name}"))
+
+        if (choiceState.Examined)
         {
             Cursor.SetCursor(_examinationKnownEvidence, Vector2.zero, CursorMode.Auto);
         }
     }
-    
+
     public void OpenExaminationMenu()
     {
         Cursor.SetCursor(_examinationNoEvidence, Vector2.zero, CursorMode.Auto);
